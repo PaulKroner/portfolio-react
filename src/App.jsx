b5e00c0d9155063d9419e12071f036950b9cb1e2
Paul Kröner <paulfriedrich.kroener@gmail.com>
2024-05-06 15:34:41 +0200

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,27 @@ import Cv from "./components/cv/cv";
 import Projects from "./components/projects/projects";
 import Skills from "./components/skills/skills";
 import Welcome from './components/welcome/welcome';
+import Questions from './components/questions/questions';
+import Footer from './components/footer/footer';
 
-function App() {
+const App = () => {
 
   // Loader Animation
-  document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-      document.querySelector(
-        "body").style.visibility = "hidden";
-      document.querySelector(
-        "#page-loader").style.visibility = "visible";
-    } else {
-      document.querySelector(
-        "#page-loader").style.display = "none";
-      document.querySelector(
-        "body").style.visibility = "visible";
-      document.querySelector(
-        "#page-loader-center").classList.remove("center");
-    }
-  };
+  // document.onreadystatechange = function () {
+  //   if (document.readyState !== "complete") {
+  //     document.querySelector(
+  //       "body").style.visibility = "hidden";
+  //     document.querySelector(
+  //       "#page-loader").style.visibility = "visible";
+  //   } else {
+  //     document.querySelector(
+  //       "#page-loader").style.display = "none";
+  //     document.querySelector(
+  //       "body").style.visibility = "visible";
+  //     document.querySelector(
+  //       "#page-loader-center").classList.remove("center");
+  //   }
+  // };
   return (
     <>
       {/* Loader */}
@@ -43,6 +45,8 @@ function App() {
               <Cv />
               <Projects />
               {/* <Skills /> */}
+              <Questions />
+              <Footer />
             </div>
           </div>
 
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import "./footer.css"
+
+const Footer = () => {
+    return (
+        <footer class="container-fluid">
+
+            <div class="footer-text">
+                Diese Webseite wurde in React.js und Bootstrap erstellt.
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer;

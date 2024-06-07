a779f7905a05f069fde20d20a45834af80247644
Paul Kröner <paulfriedrich.kroener@gmail.com>
2024-06-07 18:25:29 +0200

    Projects "mehr Anzeigen" #16

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -8,13 +8,15 @@ const Projects = () => {
             id: "long-SB",
             title: "SQL-Statementbuilder",
             content: "Der SQL-Statementbuilder ist ein einfacher Baukasten, mit dem User durch anklicken von Selectfeldern ein SQL-Statement erstellen kann, ohne die Syntax zu kennen.",
-            usedTech: ["HTML", "CSS", "JQuery", "PHP"]
+            usedTech: ["HTML", "CSS", "JQuery", "PHP"],
+            moreToShow: false
         },
         {
             id: "angularFrontend",
             title: "Angular Frontend",
             content: "Während meiner Werksstudententätigkeit habe ich den Frontend-Entwicklern ausgeholfen. Das genutzte Frontend-Framework war Angular.",
-            usedTech: ["Angular", "Angular Material", "TypeScript"]
+            usedTech: ["Angular", "Angular Material", "TypeScript"],
+            moreToShow: false
         },
         {
             id: "live-voting-tool",
@@ -24,7 +26,17 @@ const Projects = () => {
             ein Live-Voting-Tool erstellt. Ziel war es den Ablauf, Problemen und Lösungen eines Softwareprojekts 
             kennenzulernen. Der Techstack bestand aus React Typescript für das Frontend 
             und MariaDB sowie OpenAPI fürs Backend.`,
-            usedTech: ["React.js", "Bootstrap", "Typescript", "OpenAPI", "MariaDB", "Websockets"]
+            usedTech: ["React.js", "Bootstrap", "Typescript", "OpenAPI", "MariaDB", "Websockets"],
+            moreToShow: false
+        },
+        {
+            id: "little-Lemon",
+            title: "Little Lemon Restaurant",
+            content: `Im April 2024 habe ich das Meta Front-End Developer Zertifikat erhalten. 
+            Aufgabe des Kurses war die Erstellung einer Restaurant Website, auf der man einen Blick auf die Speisekarte erhalten, 
+            einen Tisch reservieren und Besucherbewertungen lesen kann.`,
+            usedTech: ["React.js", "Figma"],
+            moreToShow: true
         },
         {
             id: "poker",
@@ -32,7 +44,8 @@ const Projects = () => {
             content: `Zusammen mit einem Freund baue ich ein Multiplayer Pokerspiel, 
                     welches zum Ausbau unserer Programmier-Fähigkeiten gedacht ist. 
                     Dabei bin ich für das Frontend zuständig.`,
-            usedTech: ["React.js", "Bootstrap", "Node.js", "Socket.io"]
+            usedTech: ["React.js", "Bootstrap", "Node.js", "Socket.io"],
+            moreToShow: false
         },
     ]
     return (
@@ -47,7 +60,7 @@ const Projects = () => {
 
                 {projects.map(project => (
                     <div className="project-wrapper d-flex flex-row" key={project.id}>
-                        <ProjectsComponent id={project.id} title={project.title} content={project.content} usedTech={project.usedTech} />
+                        <ProjectsComponent id={project.id} title={project.title} content={project.content} usedTech={project.usedTech} moreToShow={project.moreToShow} />
                     </div>
 
                 ))}
